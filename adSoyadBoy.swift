ed5778cc06b4bd4021d6b7c012f84c9d1ca26c6e
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-07-05 18:17:48 +0300

    Create adSoyadBoy.swift

---
+++ adSoyadBoy.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+var name : String = "Safak"
+var surname : String = "Dal"
+var boy : Float  = 1.78
+var basHarf : Character = "S"
+
+
+print("Name:",name)
+print ("Surnama:",surname)
+print("Boy:",boy)
+print("BasHarf :",basHarf)
+
