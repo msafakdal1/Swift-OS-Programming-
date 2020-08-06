85a3c08cd6319b2bf12abb32fbb27caccb95a7de
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-08-06 10:12:44 +0300

    Create sevmek.swift

---
+++ sevmek.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+var name :String = "Merve" , soyisim :String = "Tokan"
+
+print("\(name) \(soyisim) ben seni çok seviyorum ve beni lütfen affet aşkım.Ben biraz tuhaf birisi olsamda seni çok seviyorum ve beni sevmekten asla vazgeçme asla aşkım.")
