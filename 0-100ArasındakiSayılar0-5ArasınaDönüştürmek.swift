2c2bac221253e1b6b319828d35d5bf26c0a4b529
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-08-06 14:53:43 +0300

    Create 0-100ArasındakiSayılar0-5ArasınaDönüştürmek.swift

---
+++ 0-100ArasındakiSayılar0-5ArasınaDönüştürmek.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var not = 47
+
+
+if not>=0 && not<25{
+    print("0")
+}
+else if not>=25 && not<50{
+    print("1")
+}
+else if not>=50 && not<70{
+    print("2")
+}
+else if not>=70 && not<85{
+    print("3")
+}
+else if not>=85 && not<100{
+    print("5")
+}
+else if not>=100{
+    print("5")
+}
