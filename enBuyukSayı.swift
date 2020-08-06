b46515359b1d1c0cc3ebd2c605cf0875b58fd9cf
Melih Safak <1160501801@ogrenci.kirklareli.edu.tr>
2020-08-06 15:02:49 +0300

    Create enBuyukSayı.swift

---
+++ enBuyukSayı.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var max = 0
+var sayı1 = 8
+var sayı2 = 3
+var sayı3 = 1
+
+if sayı1>max{
+    max = sayı1
+}
+if sayı2>max{
+    max = sayı2
+}
+if sayı3>max{
+    max = sayı3
+}
+print("En büyük sayı : \(max)")
